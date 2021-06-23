5de8772970a9d5342abb10b9b0317568361c5f15
Abel Souza <abelsouzacosta@gmail.com>
2021-06-22 23:33:53 -0300

    Criando um componente Button reutilizável

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,7 @@
-import { useState } from "react"
+import { ButtonHTMLAttributes } from 'react';
 
-const Button: React.FC = () => {
-  const [count, setCount] = useState(0);
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-  const incrementCounter = () => {
-    setCount(count => count += 1);
-  }
-
-  return <button onClick={incrementCounter}>{count}</button>
+export function Button(props: ButtonProps) {
+  return <button className="button" {...props} />
 }
-
-export { Button };
